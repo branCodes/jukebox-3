2ff50c7a4e1ec4ae3f5dd83dc559859f939cc1db
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-01-27 17:42:33 -0800

    text field clears upon entering song string

--- jukebox.js
+++ jukebox.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var songString = $('#song-form [type="text"]').val();
   $('#song-form-submit-button').on('click', function() {
     $('#song-queue').append('<li>' + songString + '</li>')
+    $('#song-form-text-field').val("")
   });
 });
 

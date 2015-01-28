dc709248b775a7057e9b14c0a180300618808bd5
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-01-28 09:08:51 -0800

    plays sounds now

--- jukebox.js
+++ jukebox.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
-  var songString = $('#song-form [type="text"]').val();
+
   $('#song-form-submit-button').on('click', function() {
+
+
+  var songString = $('#song-form [type="text"]').val();
+
     $('#song-queue').append('<li>' + songString + '</li>')
-    $('#song-form-text-field').val("")
-  });
+      $('#song-form-text-field').val("");
+      event.preventDefault();
+    });
+
+    $('#play-button').on('click', function() {
+      playSong(parseSong($('li:first-child').text()),100)
+    });
+
 });
 
--- parser.js
+++ parser.js
@@ -37,3 +37,28 @@ var parseSong = function(string) {
 
   return notes;
 };
+
+// ## Part 1
+
+// 1. When the song form is submitted, 
+// create a new list item in the "Song Queue" list, 
+// containing the song string. ALSO, clear the text field 
+// contents.
+
+// 2. When the "Play" button is clicked:
+//   - Start playing the top song in the queue.
+//   - Remove the top song in the queue.
+//   - When the song is finished playing, repeat 
+//   with the next top song until there are no songs 
+//   left to play.
+
+// 3. When the "Play" button is clicked, slide it up. 
+// When the last song is finished playing, slide the 
+// "Play" button back down.
+
+
+
+
+
+
+
